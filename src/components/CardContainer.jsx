e04fb0dc9a25895282540225599f6548096f1ce5
ebrar <ebrkdmr1@gmail.com>
2023-12-05 21:16:18 +0300

    filtered data

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { data } from "../helper/data";
+import PlayerCard from "./PlayerCard";
+
+const CardContainer = () => {
+  const [value, setValue] = useState("");
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const filteredFunc = (player) => {
+    const inputValue = value
+      .trim()
+      .replace(/[^a-z]/gi, "")
+      .toLowerCase();
+    if (player.name.toLowerCase().includes(inputValue)) {
+      return player;
+    }
+  };
+  return (
+    <div className="container">
+      <div>
+        <input type="search" onChange={handleChange} />
+      </div>
+
+      <div>
+        {data.filter(filteredFunc).map(({ name, img, statistics }) => (
+          <PlayerCard
+            key={name}
+            name={name}
+            img={img}
+            statistics={statistics}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardContainer;
---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PlayerCard = ({ name, img, statistics }) => {
+  return (
+    <div>
+      <img src={img} alt="" width="200px" />
+      <h4>{name}</h4>
+    </div>
+  );
+};
+
+export default PlayerCard;
